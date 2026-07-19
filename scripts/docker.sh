239259fd986370f863568fa8685efc2f62045648
Alexander Krupenkin <mail@akru.me>
2026-07-19 06:42:57 +0300

    Fix docker script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# Get the project root
 PROJECT_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
 
 docker run --name altruist -p 5900:5900 -ti -v $PROJECT_ROOT:/workdir docker.io/zephyrprojectrtos/zephyr-build:main 
